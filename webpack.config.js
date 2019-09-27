28ee41b3070f601edad87d2bad9cdafbfc3f54bc
gdesalazar67 <spencerwilliamtassone@gmail.com>
2019-09-27 13:59:54 -0400

    Fixed webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -2,34 +2,7 @@ const path = require("path");
 
 module.exports = {
   context: __dirname,
-  entry: "frontend/index.jsx",
-  output: {
-    path: path.resolve(__dirname, "app", "assets", "javascripts"),
-    filename: "app/assets/javascripts"
-  },
-  resolve: {
-    extensions: [".js", ".jsx", "*"]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          query: {
-            presets: ["@babel/env", "@babel/react"]
-          }
-        }
-      }
-    ]
-  },
-  devtool: "source-map"
-};const path = require("path");
-
-module.exports = {
-  context: __dirname,
-  entry: "./frontend/pokedex.jsx",
+  entry: "./frontend/index.jsx",
   output: {
     path: path.resolve(__dirname, "app", "assets", "javascripts"),
     filename: "bundle.js"
